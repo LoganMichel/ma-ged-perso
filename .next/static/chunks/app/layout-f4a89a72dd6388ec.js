8d9bdd18c7ff6724b4e36d06bb79dbf13884441a
Logan MICHEL <michelogan@gmail.com>
2025-12-10 17:48:14 +0100

    build: Generate Next.js application build output and configuration files.

---
+++ .next/static/chunks/app/layout-f4a89a72dd6388ec.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5807:function(n,e,i){Promise.resolve().then(i.t.bind(i,2778,23)),Promise.resolve().then(i.bind(i,141))},2778:function(){}},function(n){n.O(0,[461,141,971,117,744],function(){return n(n.s=5807)}),_N_E=n.O()}]);
